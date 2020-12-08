6b04bcab8bfafb720d0bc3dbb8266f8119970561
Hannah Robinson <hannah.robinson.git@gmail.com>
2020-12-08 14:52:27 +1300

    Prevent user from opening doors early and add day 8 content

--- script.js
+++ script.js
@@ -3,21 +3,24 @@ const calendarContainer = document.querySelector(".container")
 
 const calendarDays = 25;
 
-const openDoor = (imagePath, dayUrlPath, event) => {
-  event.target.parentNode.style.backgroundImage = `url(${imagePath})`;
-  event.target.style.opacity = "0";
-  event.target.style.backgroundColor = "#9ac1f4";
-  setTimeout(() => {
-    event.target.classList.remove("number")
-    event.target.classList.add("play-btn")
-    event.target.innerHTML = `<a href=${dayUrlPath} target=”_blank” ><i class="far fa-play-circle"></i></a>`;
-    event.target.style.opacity = "100";
-    event.target.style.backgroundColor = '';
-    event.target.removeEventListener("click", openDoor);
-  }, 2000);
-  
-  console.log(dayUrlPath);
-  console.log(imagePath);
+const openDoor = (dayNumber, imagePath, dayUrlPath, event) => {
+  let currentTime = new Date();
+  let today = currentTime.getDate();
+  if (today < dayNumber) {
+    console.log("You're too early")
+  } else {
+    event.target.parentNode.style.backgroundImage = `url(${imagePath})`;
+    event.target.style.opacity = "0";
+    event.target.style.backgroundColor = "#9ac1f4";
+    setTimeout(() => {
+      event.target.classList.remove("number")
+      event.target.classList.add("play-btn")
+      event.target.innerHTML = `<a href=${dayUrlPath} target=”_blank” ><i class="far fa-play-circle"></i></a>`;
+      event.target.style.opacity = "100";
+      event.target.style.backgroundColor = '';
+      event.target.removeEventListener("click", openDoor);
+    }, 2000);
+  }
 }
 
 const createCalendar = () => {
@@ -33,8 +36,6 @@ const createCalendar = () => {
     calendarDoorText.innerHTML = i + 1;
     calendarDoor.appendChild(calendarDoorText);
 
-    dayNumber = i + 1;
-
     const urlPaths = {
       1: "https://www.youtube.com/watch?v=s2bzP9GSnAU",
       2: "https://xkcd.com/838/",
@@ -43,13 +44,15 @@ const createCalendar = () => {
       5: "https://www.youtube.com/watch?v=uT3SBzmDxGk",
       6: "https://twitter.com/BobHagh/status/877920282710859776",
       7: "https://www.youtube.com/watch?v=n9kfdEyV3RQ",
+      8: "https://www.youtube.com/watch?v=92jbfz9qa9M",
     };
 
+    let dayNumber = i + 1;
     let dayImagePath = `./images/advent-m-${dayNumber}.jpg`;
     let dayUrlPath = urlPaths[dayNumber];
     
 
-    calendarDoorText.addEventListener("click", openDoor.bind(null, dayImagePath, dayUrlPath), {once:true});
+    calendarDoorText.addEventListener("click", openDoor.bind(null, dayNumber, dayImagePath, dayUrlPath), {once:true});
   }
 }
 
